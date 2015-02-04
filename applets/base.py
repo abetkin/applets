a339dabe62f66a60d07f153ca4aee4760fc5dae3
Vitalii Abetkin <abvit89@gmail.com>
2015-02-04 22:05:22 +0300

    stop_before stop_after

--- applets/base.py
+++ applets/base.py
@@ -1,6 +1,6 @@
 from functools import wraps
 import collections
-from contextlib import contextmanager
+from contextlib import contextmanager, ContextDecorator
 import greenlet
 
 from .util import as_context, MISSING
@@ -74,54 +74,38 @@ class GreenletWrapper:
     def __init__(self, func):
         self.__func__ = func
 
-        # why a func ?
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            g = MethodGreenlet(self)
-            g.get_context()
-            return g.switch(*args, **kwargs)
-
-        self._wrapper_func = wrapper
-
-    def get_wrapper():1
-
     @property
     def raw(self):
         return self.__func__.__get__(self.__self__)
 
     def __get__(self, instance, owner):
-        if instance:
+        if instance is not None:
             self.__self__ = instance
-        return self
+            return self
+        return self.__func__
 
     def __call__(self, *args, **kwargs):
-        method = self._wrapper_func.__get__(self.__self__)
-        return method(*args, **kwargs)
-
-    def __call__(self, *args, **kwargs):
-        return self.wait(None, *args, **kwargs)
-
-    def wait(self, func, *args, **kwargs):
         g = MethodGreenlet(self)
         g.get_context()
+        if self.__self__ is not None:
+            args = (self.__self__,) + args
         return g.switch(*args, **kwargs)
-
 
 
 class MethodGreenlet(greenlet.greenlet):
 
-    stopped = None # where to return back
-    stop_at = None
-
-    def __init__(self, method):
-        '''method is a `GreenletWrapper` instance
-        '''
-        self._method = method
+    def __init__(self, method, # stop_before=None, stop_after=None
+                ):
         super().__init__(method.__func__)
+        self._method = method
 
     @property
     def __instance__(self):
         return self._method.__self__
+
+    @property
+    def __func__(self):
+        return self._method.__func__
 
     def get_context(self):
         if hasattr(self, 'context'):
@@ -138,21 +122,85 @@ class MethodGreenlet(greenlet.greenlet):
         self.context = new
         return new
 
-    # def wait(self, func):
-    #     1
-
-    def _should_stop(self):
+    def _check(self, stop_where='stop_before'):
         g = self
+        func = self.__func__
         while g is not None:
-            1
+            if getattr(g, stop_where, None) == func:
+                return g
+            g = g.parent
+
+    def __repr__(self):
+        return 'MethodGreenlet: %s' % self.__instance__.__class__.__name__ \
+                if self.__instance__ is not None else '-'
 
     def run(self, *args, **kwargs):
-        should_stop = self.should_stop()
-        if should_stop == 'before':
-            'stop'
-        result = super().run(*args, **kwargs)
-        if should_stop == 'after':
-            g = self.get_target_g()
-            g.switch(result)
-        else:
-            return result
+        waiting = self._check('stop_before')
+        result = MISSING
+        if waiting:
+            waiting.paused_greenlet = self
+            result = waiting.switch(self.__instance__, kwargs) # TODO
+        if result is MISSING:
+            result = self.__func__(*args, **kwargs)
+
+        waiting = self._check('stop_after')
+        if waiting:
+            waiting.paused_greenlet = self
+            switched = waiting.switch(result, kwargs) # TODO
+            # if switched is not MISSING:
+            #     result = switched
+        return result
+
+
+class stop_at(ContextDecorator):
+
+    ATTRIBUTE = None
+
+    def __init__(self, where, kill=True):
+        self.func = where
+        self.kill_on_exit = kill
+        self.g_current = greenlet.getcurrent()
+
+    def __enter__(self):
+        try:
+            self.old_value = getattr(self.g_current, self.ATTRIBUTE)
+        except AttributeError:
+            pass
+        setattr(self.g_current, self.ATTRIBUTE, self.func)
+        return self
+
+    def __exit__(self, *exc):
+        self._reset_old_value()
+        if self.kill_on_exit:
+            self._kill()
+
+    def _kill(self):
+        paused = getattr(self.g_current, 'paused_greenlet', None)
+        if paused is None:
+            return
+        del self.g_current.paused_greenlet
+        while paused != self.g_current:
+            if paused:
+                paused.throw()
+            paused = paused.parent
+
+    def resume(self, result=MISSING): # TODO
+        self._reset_old_value()
+        paused = self.g_current.paused_greenlet
+        del self.g_current.paused_greenlet
+        return paused.switch(result)
+
+    def _reset_old_value(self):
+        if hasattr(self, 'old_value'):
+            setattr(self.g_current, self.ATTRIBUTE, self.old_value)
+            del self.old_value
+        try:
+            delattr(self.g_current, self.ATTRIBUTE)
+        except: pass
+
+
+class stop_before(stop_at):
+    ATTRIBUTE = 'stop_before'
+
+class stop_after(stop_at):
+    ATTRIBUTE = 'stop_after'
--- tests/test_iter.py
+++ tests/test_iter.py
@@ -1,1 +1,31 @@
 from applets.util import case
+import greenlet
+from applets.base import GreenletWrapper, stop_before
+from applets import get_from_context, context
+
+class A:
+    x = 3
+
+    @GreenletWrapper
+    def run(self):
+        return B().walk() + 1
+
+class B:
+
+    @GreenletWrapper
+    def walk(self):
+        return get_from_context('x')
+
+
+class C(A):
+
+    @GreenletWrapper
+    def run(self):
+        with stop_before(B.walk) as stopped:
+            res = super().run()
+            return stopped.resume(5)
+
+
+
+o = C()
+case.assertEqual(o.run(), 6)
