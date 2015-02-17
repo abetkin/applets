5427f45e19ffe3237219006576cf538355687368
Vitalii Abetkin <abvit89@gmail.com>
2015-02-17 22:02:28 +0300

    tmp

--- g_context/base.py
+++ g_context/base.py
@@ -1,84 +1,78 @@
 from functools import wraps
 from contextlib import contextmanager
-import greenlet
 
-from .util import Missing
-from .handles import HandleBefore, HandleAfter
+from .util import Missing, ExplicitNone
 
-from collections import ChainMap, Mapping
+from collections import Mapping
+import threading
 
 
-def getcontext():
-    # FIXME AttributeError
-    return greenlet.getcurrent().context
+threadlocal = threading.local()
+
 
 def ContextAttr(name, default=Missing):
 
     def fget(self):
-        context = getcontext()
         if default is not Missing:
             return context.get(name, default)
         return context[name]
 
     def fset(self, value):
-        context = getcontext()
         context[name] = value
 
     return property(fget, fset)
 
 
 @contextmanager
-def context(*objects):
-    g_current = greenlet.getcurrent()
-    old_ctx = getattr(g_current, 'context', None)
-    objects = list(objects)
-    if isinstance(old_ctx, ChainObjects):
-        objects.extend(old_ctx.objects)
-    elif old_ctx is not None:
-        objects.append(old_ctx)
-    g_current.context = ChainObjects(*objects)
-    yield g_current.context
-    if old_ctx is None:
-        del g_current.context
-    else:
-        g_current.context = old_ctx
+def add_context(*objects):
+    context
+    added = 0
+    for obj in objects:
+        if context.add_child(obj):
+            added += 1
+    yield
+    for i in range(added):
+        context.objects.pop()
+
 
 # TODO def replace_context() ?
 
 @Mapping.register
-class ChainObjects:
+class ObjectsChain:
+
+    pending = None
 
     def __init__(self, *objects):
         self.objects = []
-        def maps():
-            for obj in objects:
-                if obj is None or obj in self.objects:
-                    continue
-                self.objects.append(obj)
-                if isinstance(obj, Mapping):
-                    yield obj
-                    continue
-                yield obj.__dict__
-                if not isinstance(obj, type):
-                    yield obj.__class__.__dict__
-        self._chainmap = ChainMap({}, *maps())
+        for obj in objects:
+            if obj is None or obj in self.objects:
+                continue
+            self.objects.append(obj)
+        self._dic = {}
 
     def __repr__(self):
         return 'Chain ' + repr(self.objects)
 
     def __getitem__(self, key):
-        for mapping in self._chainmap.maps:
+        if key in self._dic:
+            return self._dic[key]
+        for obj in self.objects:
             try:
-                return mapping[key]
-            except KeyError:
+                if isinstance(obj, Mapping):
+                    return obj[key]
+                return getattr(obj, key)
+            except (KeyError, AttributeError):
                 pass
         return self.__missing__(key)
 
+    def __missing__(self, key):
+        raise KeyError(key)
+
     def __setitem__(self, key, value):
-        self._chainmap[key] = value
+        self._dic[key] = value
 
     def __delitem__(self, key):
-        del self._chainmap[key]
+        del self._dic[key]
 
     def __bool__(self):
         return bool(self.objects)
@@ -87,7 +81,11 @@ class ChainObjects:
         return self[key] if key in self else default
 
     def __contains__(self, key):
-        return key in self._chainmap # TODO search objects ?
+        try:
+            self[key]
+            return True
+        except KeyError:
+            pass
 
     def __iter__(self):
         yield from self.objects
@@ -96,103 +94,115 @@ class ChainObjects:
         return len(self.objects)
 
     def __eq__(self, other):
-        if isinstance(other, ChainObjects):
+        if isinstance(other, ObjectsChain):
             return self.objects == other.objects
 
     def __ne__(self, other):
         return not (self == other)
 
-    def __missing__(self, key):
-        for obj in self.objects:
-            result = getattr(obj, key, Missing)
-            if result is not Missing:
-                return result
-        raise KeyError(key)
+    def add_child(self, obj):
+        '''
+        if self.pendind:
+            pending = self.pending
+            self.pending = obj
+            self.add_child(pending)
+        if obj is not None and obj not in self.objects:
+            self.objects.insert(0, obj)
+            return True'''
 
-    def new_child(self, obj):
-        return self.__class__(obj, *self.objects)
+context = threadlocal.context = ObjectsChain()
+threadlocal.hooks = []
 
-
-class GreenletWrapper:
-    '''Wrapper for methods and functions.'''
+class ContextWrapper:
 
     def __init__(self, instance_arg=0):
         self.instance_arg = instance_arg
 
-    def _get_instance(self, *args, **kwargs):
+    def _get_instance(self, *run_args, **run_kwargs):
         if self.instance_arg is None:
             return
         try:
             index = int(self.instance_arg)
         except TypeError:
-            return kwargs[self.instance_arg]
+            return self.run_kwargs[self.instance_arg]
         else:
-            return args[index]
+            return self.run_args[index]
+
+    @contextmanager
+    def as_manager(self, *run_args, **run_kwargs):
+        instance = self._get_instance(*run_args, **run_kwargs)
+        added = context.add_child(instance)
+        yield
+        if added:
+            context.objects.pop(0)
+
+    def run(self, *args, **kwargs):
+        result = None
+        hook = threadlocal.hooks[-1] if threadlocal.hooks else None
+        if hook == (pre_hook, self.wrapper):
+            result = hook.hook_func(*args, **kwargs)
+        if result is None:
+            # *
+            result = self.func(*args, **kwargs)
+            # *
+        elif result is ExplicitNone:
+            result = None
+        if hook == (post_hook, self.wrapper):
+            ret = hook.hook_func(*args, ret=result, **kwargs)
+            if ret is ExplicitNone:
+                result = None
+            elif ret is not None:
+                result = ret
+        return result
+
 
     def __call__(self, func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            instance = self._get_instance(*args, **kwargs)
-            g = MethodGreenlet(wrapper, instance)
-            return g.switch(*args, **kwargs)
+            self.run_args = args
+            self.run_kwargs = kwargs
+            with self.as_manager(*args, **kwargs):
+                return self.run(*args, **kwargs)
+        self.func = func
+        self.wrapper = wrapper
         return wrapper
 
-green_function = GreenletWrapper(None)
-green_method = GreenletWrapper()
+
+function = ContextWrapper(None)
+method = ContextWrapper()
 
 
-class MethodGreenlet(greenlet.greenlet):
-    ''''''
+class Hook:
 
-    def __init__(self, func, instance=None):
-        self._wrapper_func = func
-        self.__self__ = instance
-        super().__init__(self.__func__)
-        # all_handles attribute is for optimization purposes
-        self.all_handles = dict(
-                (p, p) for p in getattr(self.parent, '_handles', ()))
-        self.all_handles.update(
-                getattr(self.parent, 'all_handles', ()))
-    @property
-    def __func__(self):
-        return self._wrapper_func.__wrapped__
+    def __init__(self, func, hook_func=None):
+        self.func = func
+        self.hook_func = hook_func
 
-    def _new_context(self):
-        g = self.parent
-        while g is not None and not hasattr(g, 'context'):
-            g = g.parent
-        if g is None:
-            return ChainObjects()
-        assert isinstance(g.context, ChainObjects)
-        obj = g.__self__ if isinstance(g, MethodGreenlet) else None
-        return g.context.new_child(obj)
+    def __call__(self, hook_func):
+        self.hook_func = hook_func
 
-    @property
-    def context(self):
-        if not hasattr(self, '_context'):
-            self._context = self._new_context()
-        return self._context
+    def __enter__(self):
+        threadlocal.hooks.append(self)
 
-    @context.setter
-    def context(self, value):
-        self._context = value
+    def __exit__(self, *exc):
+        if exc[0]:
+            raise exc[0].with_traceback(exc[1], exc[2])
+        threadlocal.hooks.pop()
 
-    def __repr__(self):
-        return 'MethodGreenlet: %s' % self.__self__.__class__.__name__ \
-                if self.__self__ is not None else '-'
+    def __hash__(self):
+        return hash((self.type, self.func))
 
-    def run(self, *args, **kwargs):
-        result = Missing
-        handle = self.all_handles.get((HandleBefore, self._wrapper_func))
-        if handle and handle.is_active():
-            result = handle._switch(*args, **kwargs)
-        if result is Missing:
-            # *
-            result = self.__func__(*args, **kwargs)
-            # *
-        handle = self.all_handles.get((HandleAfter, self._wrapper_func))
-        if handle and handle.is_active():
-            switched = handle._switch(*args, _result_=result, **kwargs)
-            if switched is not Missing:
-                result = switched
-        return result
+    def __eq__(self, other):
+        if isinstance(other, tuple):
+            return other == (self.type, self.func)
+        if isinstance(other, Hook):
+            return self.func is other.func and self.type is other.type
+
+
+class pre_hook(Hook):
+    pass
+pre_hook.type = pre_hook
+
+class post_hook(Hook):
+    pass
+post_hook.type = post_hook
