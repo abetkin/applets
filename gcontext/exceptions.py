91c9e636cc9e5452d7f1711248d4ac45901c0954
Vitalii Abetkin <abvit89@gmail.com>
2015-06-13 02:18:55 +0300

    go

--- gcontext/exceptions.py
+++ gcontext/exceptions.py
@@ -1,41 +1,23 @@
-#
-# class Exit(Exception):
-#     def __init__(self, **kwargs):
-#         self.__dict__.update(kwargs)
-#
-#
-# class CriticalHook(Hook):
-#
-#     def __init__(self, func):
-#         super().__init__(func)
-#         self.error = Exit()
-#
-#     def __enter__(self):
-#         super().__enter__()
-#         return self.error
-#
-#     def __exit__(self, *exc):
-#         super().__exit__(*exc)
-#         if exc and issubclass(exc[0], Exit):
-#             return True
-#
-#
-# class exit_before(CriticalHook):
-#
-#     type = pre_hook
-#
-#     def execute(self, *args, **kwargs):
-#         self.error.args = args
-#         self.error.kwargs = kwargs
-#         raise self.error
-#
-#
-# class exit_after(CriticalHook):
-#
-#     type = post_hook
-#
-#     def execute(self, *args, ret=Missing, **kwargs):
-#         self.error.args = args
-#         self.error.kwargs = kwargs
-#         self.error.ret = ret
-#         raise self.error
+from contextlib import contextmanager
+
+class Exit(Exception):
+    
+    def __init__(self):
+        1
+        # func, args, kwargs
+
+# Ex ??
+
+
+def _stop_now():
+    1
+
+@contextmanager
+def stop_at(func):
+    try:
+        yield ex # ??
+    except Exception as ex:
+        1
+
+def stop_after(func):
+    1
